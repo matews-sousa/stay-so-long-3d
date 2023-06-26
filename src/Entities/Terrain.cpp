752a21cfd90cfb9fa1fa0b1f6d572e9d7e62f7e6
Mateus Sousa <mateussdamiano@gmail.com>
2023-06-26 16:13:24 -0300

    added OpenGL lighting, being able to toggle between both methods of lighting

--- src/Entities/Terrain.cpp
+++ src/Entities/Terrain.cpp
@@ -21,25 +21,45 @@ void Terrain::draw()
   Texture::bindByName("rock");
   glm::mat4 modelMatrix = glm::mat4(1.0f);
 
-  for (int i = -size; i < size; i += tileSize)
+  if (Game::GLightsEnabled)
   {
-    for (int j = -size; j < size; j += tileSize)
+    tileSize = 50;
+    for (int i = -size; i < size; i += tileSize)
     {
-      glBegin(GL_TRIANGLE_STRIP);
+      for (int j = -size; j < size; j += tileSize)
+      {
+        glBegin(GL_TRIANGLE_STRIP);
+        glColor3f(1.0f, 1.0f, 1.0f); glTexCoord2f(0.0f, 0.0f); glNormal3f(0.0f, 1.0f, 0.0f); glVertex3f(i, 0, j);
+        glColor3f(1.0f, 1.0f, 1.0f); glTexCoord2f(0.0f, 1.0f); glNormal3f(0.0f, 1.0f, 0.0f); glVertex3f(i, 0, j + tileSize);
+        glColor3f(1.0f, 1.0f, 1.0f); glTexCoord2f(1.0f, 0.0f); glNormal3f(0.0f, 1.0f, 0.0f); glVertex3f(i + tileSize, 0, j);
+        glColor3f(1.0f, 1.0f, 1.0f); glTexCoord2f(1.0f, 1.0f); glNormal3f(0.0f, 1.0f, 0.0f); glVertex3f(i + tileSize, 0, j + tileSize);
+        glEnd();
+      }
+    }
+  }
+  else
+  {
+    tileSize = 200;
+    for (int i = -size; i < size; i += tileSize)
+    {
+      for (int j = -size; j < size; j += tileSize)
+      {
+        glBegin(GL_TRIANGLE_STRIP);
 
-      glm::vec3 illum = Light::calculateIllumination(Game::lights, glm::vec3(i, 0.0f, j), glm::vec3(0.0f, 1.0f, 0.0f), modelMatrix);
-      glColor3fv(glm::value_ptr(illum)); glTexCoord2f(0.0f, 0.0f); glNormal3f(0.0f, 1.0f, 0.0f); glVertex3f(i, 0, j);
+        glm::vec3 illum = Light::calculateIllumination(Game::lights, glm::vec3(i, 0.0f, j), glm::vec3(0.0f, 1.0f, 0.0f), modelMatrix);
+        glColor3fv(glm::value_ptr(illum)); glTexCoord2f(0.0f, 0.0f); glNormal3f(0.0f, 1.0f, 0.0f); glVertex3f(i, 0, j);
 
-      illum = Light::calculateIllumination(Game::lights, glm::vec3(i, 0.0f, j + tileSize), glm::vec3(0.0f, 1.0f, 0.0f), modelMatrix);
-      glColor3fv(glm::value_ptr(illum)); glTexCoord2f(0.0f, 1.0f); glNormal3f(0.0f, 1.0f, 0.0f); glVertex3f(i, 0, j + tileSize);
+        illum = Light::calculateIllumination(Game::lights, glm::vec3(i, 0.0f, j + tileSize), glm::vec3(0.0f, 1.0f, 0.0f), modelMatrix);
+        glColor3fv(glm::value_ptr(illum)); glTexCoord2f(0.0f, 1.0f); glNormal3f(0.0f, 1.0f, 0.0f); glVertex3f(i, 0, j + tileSize);
 
-      illum = Light::calculateIllumination(Game::lights, glm::vec3(i + tileSize, 0.0f, j), glm::vec3(0.0f, 1.0f, 0.0f), modelMatrix);
-      glColor3fv(glm::value_ptr(illum)); glTexCoord2f(1.0f, 0.0f); glNormal3f(0.0f, 1.0f, 0.0f); glVertex3f(i + tileSize, 0, j);
-      
-      illum = Light::calculateIllumination(Game::lights, glm::vec3(i + tileSize, 0.0f, j + tileSize), glm::vec3(0.0f, 1.0f, 0.0f), modelMatrix);
-      glColor3fv(glm::value_ptr(illum)); glTexCoord2f(1.0f, 1.0f); glNormal3f(0.0f, 1.0f, 0.0f); glVertex3f(i + tileSize, 0, j + tileSize);
-      
-      glEnd();
+        illum = Light::calculateIllumination(Game::lights, glm::vec3(i + tileSize, 0.0f, j), glm::vec3(0.0f, 1.0f, 0.0f), modelMatrix);
+        glColor3fv(glm::value_ptr(illum)); glTexCoord2f(1.0f, 0.0f); glNormal3f(0.0f, 1.0f, 0.0f); glVertex3f(i + tileSize, 0, j);
+        
+        illum = Light::calculateIllumination(Game::lights, glm::vec3(i + tileSize, 0.0f, j + tileSize), glm::vec3(0.0f, 1.0f, 0.0f), modelMatrix);
+        glColor3fv(glm::value_ptr(illum)); glTexCoord2f(1.0f, 1.0f); glNormal3f(0.0f, 1.0f, 0.0f); glVertex3f(i + tileSize, 0, j + tileSize);
+        
+        glEnd();
+      }
     }
   }
 }
